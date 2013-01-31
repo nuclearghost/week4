92994adec2416df372a88e1e7d97e392be18a720
Jeff Cohen <jeff@codeacademy.org>
2013-01-30 20:29:33 -0600

    All 7, with resources line in the routes

--- code/imdb/app/controllers/directors_controller.rb
+++ code/imdb/app/controllers/directors_controller.rb
@@ -1,8 +1,15 @@
 class DirectorsController < ApplicationController
 
+  def destroy
+    @director = Director.find_by_id(params[:id])
+    @director.destroy
+    redirect_to directors_url
+  end
+
   def edit
     @director = Director.find_by_id(params[:id])
   end
+
   def update
     @director = Director.find_by_id(params[:id])
     @director.name = params[:director][:name]
@@ -12,6 +19,11 @@ class DirectorsController < ApplicationController
 
   def index
     @directors = Director.all
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @directors }
+    end
   end
 
   def show
@@ -24,9 +36,10 @@ class DirectorsController < ApplicationController
 
   # {"utf8"=>"✓", "authenticity_token"=>"RsT3EZqXvYXArZmBaTLwU1fxUIEkIh5kBiYlH/Ga750=", "director"=>{"name"=>"Me"}, "commit"=>"Create Director"}
 
+  # { "name" => "Steven Spielberg"}
   def create
-    @director = Director.new
-    @director.name = params["director"]["name"]
+    @director = Director.new(params[:director])
+    # @director.name = params["director"]["name"]
     @director.save
     redirect_to directors_url
   end
--- code/imdb/config/routes.rb
+++ code/imdb/config/routes.rb
@@ -1,11 +1,16 @@
 Imdb::Application.routes.draw do
 
-  get "/directors/new" => 'directors#new', :as => 'new_director'
+  resources :directors
 
-  get "/directors" => 'directors#index', :as => 'directors'
-  get "/directors/:id" => 'directors#show', :as => 'director'
-  post "/directors" => 'directors#create', :as => 'directors'
 
-  get "/directors/:id/edit" => 'directors#edit', :as => 'edit_director'
-  put "/directors/:id" => 'directors#update', :as => 'director'
+  # get "/directors/new" => 'directors#new', :as => 'new_director'
+
+  # get "/directors" => 'directors#index', :as => 'directors'
+  # get "/directors/:id" => 'directors#show', :as => 'director'
+  # post "/directors" => 'directors#create', :as => 'directors'
+
+  # get "/directors/:id/edit" => 'directors#edit', :as => 'edit_director'
+  # put "/directors/:id" => 'directors#update'
+
+  # delete "/directors/:id" => 'directors#destroy'
 end
